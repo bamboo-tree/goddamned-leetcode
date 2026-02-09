13c1c7da72c2cf5fca994f471f57d07292592ec4
Maciej Kamiński <maciej.kam03@gmail.com>
2026-02-09 11:45:16 +0100

    add: my solution
    for some reason i'm not able to compile it in leetcode. but i've tested
    it and can proudly say "it works on my machine" :)

---
+++ longest-palindroming-substring/main.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+char *longestPalindrome(char *s);
+
+int main() {
+    
+    char *string = "babad";
+    
+    char* result = longestPalindrome(string);
+    printf("%s - result: %s\n", string, result);
+
+    return 0;
+}
+
+char *longestPalindrome(char *s) {
+    int size = 0;
+    while(s[size] != '\0') {
+        size++;
+    }
+
+    // size of tested substring
+    for(int i = size; i > 0; i--) {
+        // move the substring "frame" through base string
+        for(int j = 0; j < size-i+1; j++) {
+            bool isPalindrome = true;
+            // printf("range: [%d, %d]\n", j, i+j-1);
+            for(int k = j; k <= i+j-1; k++) {
+                int start = k;
+                int end = i+j-1-k+j; // that was hard... :')
+                // printf("[%d] vs [%d]\n", start, end);
+                if(s[start] != s[end]) {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+            
+            // allocate memory and return first palindrome (it's also the biggest one)
+            if(isPalindrome) {
+                char *result = (char *)malloc(sizeof(char) * i);
+                for(int k = 0; k < i; k++) {
+                    result[k] = s[k+j];
+                }
+
+                return result;
+            }
+        }
+    }
+
+    return "";
+}
+
